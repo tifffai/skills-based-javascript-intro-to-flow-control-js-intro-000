54d154039b9d1ed9306cbb3153e414390020f233
Tiffany Fai <tbluebox@gmail.com>
2019-10-28 12:34:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,12 +27,12 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age) {
-    case >= 13 && <= 19:
-      return "You are a teenager";
-      break;
-    default:
-      return "You have an age";
-      break;
-  }
+  // switch(age) {
+  //   case >= 13 && <= 19:
+  //     return "You are a teenager";
+  //     break;
+  //   default:
+  //     return "You have an age";
+  //     break;
+  // }
 }
