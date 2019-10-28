23210f42c1b0a88f01094b8b37ba5ee87017b4b9
Tiffany Fai <tbluebox@gmail.com>
2019-10-28 12:59:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,10 +28,10 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case (< 13):
+    case (age < 13):
       return "You have an age"
       break;
-    case (>= 13 && <= 19):
+    case (age >= 13 && age <= 19):
       return "You are a teenager"
       break;
     default:
